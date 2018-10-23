89f15ac4629b79ee3c219e46ce6c4848959034ce
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-23 11:25:33 -0700

    added BoardComponent route and put destroy in seeds to reset
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Board.destroy_all
+List.destroy_all
+Card.destroy_all
+
 board1 = Board.create title: 'board1'
 list1 = List.create title: 'board1', board: board1
 card1 = Card.create title: 'card1', board: board1, list: list1
