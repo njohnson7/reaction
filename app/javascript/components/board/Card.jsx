1b96eecbffb89f3f9b083981ad9c9cdc7acb9b1c
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-25 10:48:21 -0700

    fixed npm

--- app/javascript/components/board/Card.jsx
+++ app/javascript/components/board/Card.jsx
@@ -5,19 +5,14 @@ const Card = ({ card }) => (
   <div className='card-background'>
     <div className="card ">
       <i className="edit-toggle edit-icon sm-icon"></i>
-
       <div className="card-info">
         {card.labels.map((label, i) => (
           <div key={i} className={`card-label ${label} colorblindable`}></div>
         ))}
         <p>{card.title}</p>
       </div>
-
-      <h1>asdasdasjukhdiasd</h1>
-
       <div className="card-icons">
         <i className="clock-icon sm-icon overdue-recent completed">
-          {moment(card.due_date).format('MM-DD-YYYY')}
           {moment(card.due_date).format('MM-DD-YYYY')}
         </i>
         <i className="description-icon sm-icon"></i>
