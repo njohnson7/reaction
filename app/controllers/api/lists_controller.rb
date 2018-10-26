2708dbf140140356cf9faac31ef01310608f19dc
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-10-26 12:57:50 -0400

    fix PUT request to api/list/:id Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com> Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local> Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -28,20 +28,25 @@ class Api::ListsController < ApplicationController
   end
 
   def update
+    require 'pry'; binding.pry
     @list = List.find(params[:id])
 
-    if @list.update(list_params)
+    if @list.update(id: params[:id], board_id: @list.board_id, title: params[:title])
       render :update
     else
       @error = @list.errors.full_message.join(', ')
       render 'api/shared/error', status: :unprocessable_entity
     end
-  rescue ActiveRecord::ParamaterMissing
+  rescue ActionController::ParameterMissing
     @error = 'Invalid list data provided'
     render 'api/shared/error', status: :not_found
   end
 
   private
+
+  # def title_params
+  #   params.require(:list).permit(:title)
+  # end
 
   def list_params
     params.permit(list: :title)[:list].merge({board_id: params.require(:board_id)})
