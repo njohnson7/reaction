28efe22c0a6feab6cd4c78cf8bce5518dc379794
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-26 12:12:01 -0700

    added create list functionality
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- app/javascript/components/board/ListContainer.jsx
+++ app/javascript/components/board/ListContainer.jsx
@@ -1,19 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import List from './List'
+import AddList from './AddList'
 
-const ListContainer = ({ lists }) => (
-  <div id="list-container" className="list-container">
-    <div id="existing-lists" className="existing-lists">
-      {lists.map(list => (<List key={list.id} list={list} />))}
-    </div>
-    <div id="new-list" className="new-list">
-      <span>Add a list...</span>
-      <input type="text" placeholder="Add a list..." />
-      <div>
-        <input type="submit" className="button" value="Save" /><i className="x-icon icon"></i>
+// import * as actions from '../../actions/BoardActions';
+// import { fetchBoard } from '../../actions/BoardActions';
+
+class ListContainer extends React.Component {
+  state = {
+    addListVisible: false,
+    addListText: '',
+  }
+
+  addListClickHandler = () => {
+    this.setState({ addListVisible: !this.state.addListVisible })
+  }
+  addListChangeHandler = (event) => {
+    this.setState({ addListText: event.target.value })
+  }
+
+  addListSaveHandler = (event) => {
+    console.log(event, this)
+    event.preventDefault()
+    if (!this.state.addListText) return
+
+
+    let board_id = location.pathname.match(/boards\/(\d+)/)[1]
+    let title = this.state.addListText
+    let body = JSON.stringify({
+      board_id,
+      list: { title },
+    })
+
+    fetch('/api/lists', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    })
+      .then(resp => console.log(resp))
+      // Render new list onto page
+      // .then(_ => this.context.store.dispatch(actions.fetchBoard(boardId)))
+
+    console.log(this.state.addListText)
+    this.setState({ 
+      addListVisible: false,
+      addListText: '',
+    })
+  }
+
+  render() {
+    return (
+      <div id="list-container" className="list-container">
+        <div id="existing-lists" className="existing-lists">
+          {this.props.lists.map(list => (
+            <List key={list.id} list={list} />
+          ))}
+        </div>
+        <AddList
+          visible={this.state.addListVisible}
+          click={this.addListClickHandler}
+          change={this.addListChangeHandler}
+          text={this.state.addListText}
+          save={this.addListSaveHandler}
+        />
       </div>
-    </div>
-  </div>
-);
+    )
+  }
+}
 
 export default ListContainer;
---
+++ pivotal-tracker/user-can-add-a-list-to-a-board.md
@@ -0,0 +1,11 @@
+As a user
+When I'm on the board view page
+
+When I click on the "Add a List..." button
+I should see the button turn into an input field with "Add a List..." placeholder text
+And I fill the field with a new list's title
+And I click on the "Save" button
+The page should have an empty list with the title that I put in as the last list
+And there should be an "Add a List" button after the last list on the page
+
+Note: if the input is an empty string, the form doesn't disappear when we click the "Save" button.
