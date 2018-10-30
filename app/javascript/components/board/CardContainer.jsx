909008199aa59b7a63792353f64fdcc404db3e67
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2018-10-30 10:04:50 -0400

    remove console log Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com> Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local> Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- app/javascript/components/board/CardContainer.jsx
+++ app/javascript/components/board/CardContainer.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 const CardContainer = ({ listId }, context) => (
   <div id="cards-container" data-id="list-1-cards">
-    {console.log(context.store.getState().cards, listId)}
     {context.store.getState().cards.map(card => {
       if (card.list_id === +listId) {
         return (<Card card={card} key={card.id} />);
